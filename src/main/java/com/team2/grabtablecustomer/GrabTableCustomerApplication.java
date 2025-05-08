a69846df4a9ea2ab8653f9a293413e8ff3b2856e
Siheon Kim <siheon0411@gmail.com>
2025-05-08 14:48:51 +0900

    first commit

---
+++ src/main/java/com/team2/grabtablecustomer/GrabTableCustomerApplication.java
@@ -0,0 +1,13 @@
+package com.team2.grabtablecustomer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrabTableCustomerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrabTableCustomerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/team2/grabtablecustomer/GrabTableCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team2.grabtablecustomer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrabTableCustomerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
