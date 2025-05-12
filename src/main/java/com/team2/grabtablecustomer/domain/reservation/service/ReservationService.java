d96b5246b8875a7f2bb015dc379dbcec2b92a8d4
HSH-11 <lyk1517@naver.com>
2025-05-12 17:33:22 +0900

    feat: 등급별 예약 시간대 조회 기능 추가

--- src/main/java/com/team2/grabtablecustomer/domain/reservation/service/ReservationService.java
+++ src/main/java/com/team2/grabtablecustomer/domain/reservation/service/ReservationService.java
@@ -1,20 +1,10 @@
 package com.team2.grabtablecustomer.domain.reservation.service;
 
-import com.team2.grabtablecustomer.domain.reservation.dto.ReservationRequestDto;
-import com.team2.grabtablecustomer.domain.reservation.dto.ReservationResponseDto;
+import com.team2.grabtablecustomer.domain.reservation.dto.ReservationSlotResponseDto;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.List;
 
 public interface ReservationService {
 
-    void createReservation(String userEmail, ReservationRequestDto dto);
-
-    List<ReservationResponseDto> getMyReservations(String userEmail);
-
-    List<LocalTime> getGoldAvailableTimes(Long storeId, LocalDate date);
-    List<LocalTime> getSilverAvailableTimes(Long storeId, LocalDate date);
-    List<LocalTime> getBronzeAvailableTimes(Long storeId, LocalDate date);
-
+    List<ReservationSlotResponseDto> getAvailableSlotsByGrade(Long storeId, String grade);
 }
